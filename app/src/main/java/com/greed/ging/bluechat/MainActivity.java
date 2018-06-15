8d132f1df3215e44918a16a583cbd59f266516a3
ging7 <g776808@gmail.com>
2018-06-15 21:57:51 +0800

    Initial commit

---
+++ app/src/main/java/com/greed/ging/bluechat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.greed.ging.bluechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
